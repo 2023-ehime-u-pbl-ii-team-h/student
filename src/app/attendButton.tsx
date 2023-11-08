40cdaa3ee78efa550d2d1ea856c807bbf47e75a5
i004asai <haru.no.8610@icloud.com>
2023-11-09 08:59:43 +0900

    クリックハンドラを受け取る処理の追加

--- src/app/attendButton.tsx
+++ src/app/attendButton.tsx
@@ -1,33 +1,34 @@
 import aButton from './attendButton.module.css'
-import {BsCheckLg} from 'react-icons/bs'
-import {RxCross1} from 'react-icons/rx'
+import { BsCheckLg } from 'react-icons/bs'
+import { RxCross1 } from 'react-icons/rx'
 
-interface AttendButtonProps{
+interface AttendButtonProps {
     state: number;
+    onClick: () => void;
 }
 
-export default function AttendButton({state}: AttendButtonProps) {
-    let label;
-    let btnClass = '';
-    let iconConponent = null;
-    if(state === 1){
-        label ='出席申請する';
-        btnClass='apply';
-    }else if(state === 2){
-        label = '出席済み';
-        btnClass = 'applied';
-        iconConponent = <BsCheckLg className={aButton.icon} />;
-    }else if(state === 3){
-        label = '申請時間外';
-        btnClass = 'overtime';
-        iconConponent = <RxCross1 className={aButton.icon} />
+    export default function AttendButton({ state, onClick }: AttendButtonProps) {
+        let label;
+        let btnClass = '';
+        let iconConponent = null;
+        if (state === 1) {
+            label = '出席申請する';
+            btnClass = 'apply';
+        } else if (state === 2) {
+            label = '出席済み';
+            btnClass = 'applied';
+            iconConponent = <BsCheckLg className={aButton.icon} />;
+        } else if (state === 3) {
+            label = '申請時間外';
+            btnClass = 'overtime';
+            iconConponent = <RxCross1 className={aButton.icon} />;
+        }
+        return (
+            <div className={aButton.button} onClick={onClick}>
+                <div className={aButton[btnClass]}>
+                    {iconConponent}
+                    <span className={aButton.label}>{label}</span>
+                </div>
+            </div>
+        )
     }
-    return (
-        <div>
-        <button className={aButton[btnClass]}>
-            {iconConponent}
-            <span className={aButton.label}>{label}</span>
-        </button>
-        </div>
-    )
-}
