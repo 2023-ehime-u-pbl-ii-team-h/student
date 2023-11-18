39d8dadeb51a09cf020b1214741f82815960da98
i116iked <138637500+i116iked@users.noreply.github.com>
2023-11-18 13:15:22 +0900

    Update src/molecules/top-navigation-bar.jsx
    Co-authored-by: Mikuroさいな <ryosukadnak@gmail.com>

--- src/molecules/top-navigation-bar.jsx
+++ src/molecules/top-navigation-bar.jsx
@@ -20,7 +20,7 @@ import styles from './top-navigation-bar.module.css';
 
   const TopNavBar = ({ isLoggedIn, userInitial }) => {
   return (
-    <div className={`topNavBar surface on-surface-text`}>
+    <div className={`${styles.topNavBar} surface on-surface-text`}>
       <HamburgerIcon />
       <CurrentScreenLabel label="ホーム" />
       <UserAvatar isLoggedIn={isLoggedIn} userInitial={userInitial} />
