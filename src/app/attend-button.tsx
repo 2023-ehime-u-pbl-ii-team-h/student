c150f8756951c56734c106263c280b53abb1fbfb
asaiharuto <haru.no.8610@icloud.com>
2023-11-20 11:17:00 +0900

    Flex Layout で間隔を指定するように修正

--- src/app/attend-button.tsx
+++ src/app/attend-button.tsx
@@ -9,28 +9,30 @@ export type AttendButtonProps = {
     onClick: () => void;
 }
 
-    export default function AttendButton({ state, onClick }: AttendButtonProps) {
-        let label;
-        let btnClass = '';
-        let iconConponent = null;
-        if (state === 'ENABLED') {
-            label = '出席申請する';
-            btnClass = 'enabled';
-        } else if (state === 'DONE') {
-            label = '出席済み';
-            btnClass = 'done';
-            iconConponent = <BsCheckLg className={aButton.icon} />;
-        } else if (state === 'OVERTIME') {
-            label = '申請時間外';
-            btnClass = 'overtime';
-            iconConponent = <RxCross1 className={aButton.icon} />;
-        }
-        return (
-            <div className={aButton.button} onClick={onClick}>
-                <div className={aButton[btnClass]}>
-                    {iconConponent}
-                    <span className={aButton.label}>{label}</span>
-                </div>
-            </div>
-        )
+export default function AttendButton({ state, onClick }: AttendButtonProps) {
+    let label;
+    let btnClass = '';
+    let iconConponent = null;
+    if (state === 'ENABLED') {
+        label = '出席申請する';
+        btnClass = 'enabled';
+    } else if (state === 'DONE') {
+        label = '出席済み';
+        btnClass = 'done';
+        iconConponent = <BsCheckLg className={aButton.icon} />;
+    } else if (state === 'OVERTIME') {
+        label = '申請時間外';
+        btnClass = 'overtime';
+        iconConponent = <RxCross1 className={aButton.icon} />;
     }
+    return (
+        <div className={aButton.button} onClick={onClick}>
+            <div className={aButton[btnClass]}>
+                {iconConponent}
+            </div>
+            <div>
+                <span className={aButton[btnClass]}>{label}</span>
+            </div>
+        </div>
+    )
+}
