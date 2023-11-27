8a6a59b54408bbc76a2a2bc1083886e5d53566e0
i116iked <138637500+i116iked@users.noreply.github.com>
2023-11-27 12:23:03 +0900

    コメントに基づいて修正

--- src/app/redirect/page.tsx
+++ src/app/redirect/page.tsx
@@ -1,70 +1,29 @@
 import { useState, useEffect } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/router';
 import styles from './RedirectPage.module.css';
+import { fetchAccessToken } from '../../commands/msal-token';
 
 const RedirectPage = () => {
     const router = useRouter();
-    const searchParams = useSearchParams();
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        const authCode = searchParams.get("authorization_code");
+        const authCode = router.query.authorization_code;
 
-        if (authCode) {
-            fetchAccessToken(authCode)
-                .then(() => router.push('/'))
-                .catch(err => {
-                    setError('ログインに失敗しました。もう一度お試しください。');
-                });
-        } else {
+        if (!authCode) {
             setError('無効な認証コードです。ログインを再度お試しください。');
+            return;
         }
+
+        fetchAccessToken(authCode)
+            .then(() => {
+                // トークンの取得に成功した場合の処理
+                router.push('/');
+            })
+            .catch(err => {
+                setError('ログインに失敗しました。もう一度お試しください。');
+            });
     }, [router]);
-
-
-    const fetchAccessToken = async (authCode) => {
-        try {
-            const microsoftTokenResponse = await fetch('MICROSOFT_TOKEN_ENDPOINT', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                },
-                body: new URLSearchParams({
-                    // パラメータは実際の環境に合わせて設定
-                    'client_id': 'YOUR_CLIENT_ID',
-                    'scope': 'openid email profile',
-                    'code': authCode,
-                    'redirect_uri': 'YOUR_REDIRECT_URI',
-                    'grant_type': 'authorization_code',
-                }),
-            });
-    
-            if (!microsoftTokenResponse.ok) {
-                throw new Error('Failed to fetch Microsoft access token');
-            }
-    
-            const microsoftToken = await microsoftTokenResponse.json();
-    
-            const backendLoginResponse = await fetch('YOUR_BACKEND_LOGIN_ENDPOINT', {
-                method: 'POST',
-                headers: {
-                    'Authorization': `Bearer ${microsoftToken.access_token}`,
-                },
-            });
-    
-            if (!backendLoginResponse.ok) {
-                throw new Error('Failed to login to the backend');
-            }
-    
-            const tokens = await backendLoginResponse.json();
-            return tokens;
-    
-        } catch (error) {
-            console.error('Error fetching access token:', error);
-            throw error;
-        }
-    };
-    
 
     return (
         <div>
---
+++ src/commands/msal-token.ts
@@ -0,0 +1,42 @@
+const MICROSOFT_TOKEN_ENDPOINT = 'MicrosoftトークンエンドポイントURL';
+const BACKEND_LOGIN_ENDPOINT = '/loginエンドポイントURL';
+
+export const fetchAccessToken = async (authCode) => {
+    try {
+        const microsoftTokenResponse = await fetch(MICROSOFT_TOKEN_ENDPOINT, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: new URLSearchParams({
+                'client_id': 'YOUR_CLIENT_ID', // 実際のクライアントID
+                'scope': 'openid email profile',
+                'code': authCode,
+                'redirect_uri': 'YOUR_REDIRECT_URI', // 実際のリダイレクトURI
+                'grant_type': 'authorization_code',
+            }),
+        });
+
+        if (!microsoftTokenResponse.ok) {
+            throw new Error('Failed to fetch Microsoft access token');
+        }
+
+        const microsoftToken = await microsoftTokenResponse.json();
+
+        const backendLoginResponse = await fetch(BACKEND_LOGIN_ENDPOINT, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${microsoftToken.access_token}`,
+            },
+        });
+
+        if (!backendLoginResponse.ok) {
+            throw new Error('Failed to login to the backend');
+        }
+        
+        return;
+    } catch (error) {
+        console.error('Error in fetchAccessToken:', error);
+        throw error;
+    }
+};
