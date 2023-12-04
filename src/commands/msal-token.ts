15c923516ad3383a12c42fb179cb30442da2acd6
i116iked <138637500+i116iked@users.noreply.github.com>
2023-12-04 11:42:16 +0900

    tryをのけてそのまま書く

--- src/commands/msal-token.ts
+++ src/commands/msal-token.ts
@@ -1,40 +1,36 @@
 const MICROSOFT_TOKEN_ENDPOINT = 'https://login.microsoftonline.com/organizations/oauth2/v2.0/token';
 const BACKEND_LOGIN_ENDPOINT = 'https://backend.mikuroxina.workers.dev/login';
+const CLIENT_ID = '788aebee-7aa0-4286-b58c-7e35bf22e92a';
 
 export const fetchAccessToken = async (authCode) => {
-    try {
-        const microsoftTokenResponse = await fetch(MICROSOFT_TOKEN_ENDPOINT, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body: new URLSearchParams({
-                'client_id': 'YOUR_CLIENT_ID', // 実際のクライアントID
-                'scope': 'user.read mail.read',
-                'code': authCode,
-                'redirect_uri': window.location.origin + window.location.pathname,
-                'grant_type': 'authorization_code',
-            }),
-        });
+    const microsoftTokenResponse = await fetch(MICROSOFT_TOKEN_ENDPOINT, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({
+            'client_id': CLIENT_ID,
+            'scope': 'user.read mail.read',
+            'code': authCode,
+            'redirect_uri': window.location.origin + window.location.pathname,
+            'grant_type': 'authorization_code',
+        }),
+    });
 
-        if (!microsoftTokenResponse.ok) {
-            throw new Error('Failed to fetch Microsoft access token');
-        }
+    if (!microsoftTokenResponse.ok) {
+        throw new Error('Failed to fetch Microsoft access token');
+    }
 
-        const microsoftToken = await microsoftTokenResponse.json();
+    const microsoftToken = await microsoftTokenResponse.json();
 
-        const backendLoginResponse = await fetch(BACKEND_LOGIN_ENDPOINT, {
-            method: 'POST',
-            headers: {
-                'Authorization': `Bearer ${microsoftToken.access_token}`,
-            },
-        });
+    const backendLoginResponse = await fetch(BACKEND_LOGIN_ENDPOINT, {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${microsoftToken.access_token}`,
+        },
+    });
 
-        if (!backendLoginResponse.ok) {
-            throw new Error('Failed to login to the backend');
-        }
-    } catch (error) {
-        console.error('Error in fetchAccessToken:', error);
-        throw error;
+    if (!backendLoginResponse.ok) {
+        throw new Error('Failed to login to the backend');
     }
 };
