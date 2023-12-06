93b7c31d85458f13457758028b7dccbf622b8820
asaiharuto <haru.no.8610@icloud.com>
2023-12-07 08:37:57 +0900

    MsalProviderの追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../theme/theme.css";
+import { MsalProvider } from "@azure/msal-react";
 
 const inter = Inter({ subsets: ["latin"] });
 
