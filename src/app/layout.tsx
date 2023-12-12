015d221478835a0287550f3029aa1601a3f91bb9
asaiharuto <haru.no.8610@icloud.com>
2023-12-12 15:58:20 +0900

    provider.tsxの追加とそれに伴う修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,18 +2,9 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../theme/theme.css";
-import { MsalProvider } from "@azure/msal-react";
-import { Configuration, PublicClientApplication } from "@azure/msal-browser";
+import { Providers } from "./provider";
 
 const inter = Inter({ subsets: ["latin"] });
-
-const configuration: Configuration = {
-  auth: {
-    clientId: "788aebee-7aa0-4286-b58c-7e35bf22e92a",
-  },
-};
-
-const pca = new PublicClientApplication(configuration);
 
 export const metadata: Metadata = {
   title: "出席確認システム - 学生用",
@@ -28,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body className={inter.className}>
-        <MsalProvider instance={pca}>{children}</MsalProvider>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
