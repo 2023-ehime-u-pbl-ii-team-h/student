d68f0c78ad9a8bfdc77ccce14cc7e8074709228e
asaiharuto <haru.no.8610@icloud.com>
2023-12-13 17:21:05 +0900

    レビューに基づいた修正

--- src/commands/logout-action.ts
+++ src/commands/logout-action.ts
@@ -2,24 +2,16 @@ const BACKEND_LOGOUT_ENDPOINT = "https://backend.mikuroxina.workers.dev/logout";
 
 export const logoutAndClearAuth = async () => {
   try {
-    // ログアウトエンドポイントにアクセス
     const response = await fetch(BACKEND_LOGOUT_ENDPOINT, {
-      method: "POST", // ログアウトエンドポイントによってはPOSTを使用
-      credentials: "include", // クッキーを送信するためにcredentialsを設定
+      method: "POST",
+      // これは異なるオリジンへのリクエストだが Cookie を送信してほしいので, include を設定
+      credentials: "include",
     });
 
     if (response.ok) {
-      // ログアウトが成功したら、HttpOnlyのCookieをクリア
-      document.cookie.split(";").forEach(function (c) {
-        document.cookie = c
-          .replace(/^ +/, "")
-          .replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/");
-      });
-
-      // ページをリロードするか、リダイレクトするなどの処理を行う
       window.location.reload();
     } else {
-      console.error("Logout failed");
+      response.text().then(console.error);
     }
   } catch (error) {
     console.error("An error occurred during logout", error);
