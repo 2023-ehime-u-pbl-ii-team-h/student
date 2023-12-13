b42a8bc0cf81e9ecb7fa651ad1188dfa6f2808a2
asaiharuto <haru.no.8610@icloud.com>
2023-12-13 11:03:05 +0900

    出席のアクションを追加する

---
+++ src/organisms/attend-action.tsx
@@ -0,0 +1,37 @@
+import { useState, useCallback } from "react";
+
+const ATTNED_API_ENDPOINT = "https://example.com/attend";
+
+export type AttendResult =
+  | { type: "READY" }
+  | { type: "AWAITING" }
+  | { type: "SUCCESS"; response: Response }
+  | { type: "FAILURE" };
+
+export function useAttendAction() {
+  const [result, setResult] = useState<AttendResult>({ type: "READY" });
+
+  const submit = useCallback(async () => {
+    const submitAt = new Date();
+    setResult({ type: "AWAITING" });
+
+    const response = await fetch(ATTNED_API_ENDPOINT, {
+      method: "BODY",
+      body: JSON.stringify({
+        // ...
+      }),
+    });
+    if (!response.ok) {
+      setResult({
+        type: "FAILURE",
+      });
+      return;
+    }
+    setResult({
+      type: "SUCCESS",
+      response,
+    });
+  }, [result]);
+
+  return [result, submit];
+}
