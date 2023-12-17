4005b1a24d3753ecb81a3791965def470c450a86
i116iked <138637500+i116iked@users.noreply.github.com>
2023-12-17 16:18:45 +0900

    Update src/molecules/side-menu.tsx
    Co-authored-by: Mikuroさいな <ryosukadnak@gmail.com>

--- src/molecules/side-menu.tsx
+++ src/molecules/side-menu.tsx
@@ -9,16 +9,22 @@ const SideMenu = ({ isOpen, closeMenu, subjects }) => {
           <span className="title-small on-primary-text">出席確認システム</span>
           <button className={`${styles.closeButton} on-primary`} onClick={closeMenu}>×</button>
         </div>
-        <button className={`${styles.menuItemButton} on-background-text`}>ホーム</button>
+        <button className={`${styles.menuItemButton} on-background-text`}>
+          <div className={styles.stateLayer}>ホーム</div>
+        </button>
         <div className={styles.subjectList}>
           {subjects && subjects.map((subject, index) => (
-            <button key={index} className={`${styles.menuItemButton} secondary-container secondary-container-text`}>
-              <div className={styles.subjectName}>{subject.name}</div>
-              <div className={styles.lastDate}>{subject.lastDate}</div>
+            <button key={index} className={`${styles.menuItemButton} secondary-container on-secondary-container-text`}>
+              <div className={styles.stateLayer}>
+                <div className={styles.subjectName}>{subject.name}</div>
+                <div className={styles.lastDate}>{subject.lastDate}</div>
+              </div>
             </button>
           ))}
         </div>
-        <button className={`${styles.menuItemButton} on-background-text`}>科目を追加</button>
+        <button className={`${styles.menuItemButton} on-background-text`}>
+          <div className={styles.stateLayer}>科目を追加</div>
+        </button>
 
       </div>
     </>
