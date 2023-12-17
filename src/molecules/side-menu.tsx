c9a4776b6038f808e017cdc941a47a35d06a51f2
i116iked <138637500+i116iked@users.noreply.github.com>
2023-12-17 16:19:31 +0900

    Update src/molecules/side-menu.tsx
    Co-authored-by: Mikuroさいな <ryosukadnak@gmail.com>

--- src/molecules/side-menu.tsx
+++ src/molecules/side-menu.tsx
@@ -15,8 +15,8 @@ const SideMenu = ({ isOpen, closeMenu, subjects }: SideMenuProps) => {
       {isOpen && <div className={`${styles.overlay} scrim`} onClick={closeMenu}></div>}
       <div className={`${styles.sideMenu} surface`} data-open={isOpen}>
         <div className={`${styles.topBar} on-surface-text`}>
-          <span className="title-small on-primary-text">出席確認システム</span>
-          <button className={`${styles.closeButton} on-primary`} onClick={closeMenu}>×</button>
+          <span className="title-small">出席確認システム</span>
+          <button className={styles.closeButton} onClick={closeMenu}>×</button>
         </div>
         <button className={`${styles.menuItemButton} on-background-text`}>
           <div className={styles.stateLayer}>ホーム</div>
