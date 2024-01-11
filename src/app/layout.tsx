df39a5eaec712fe9be3965403f11c9518f51765a
MikuroXina <ryosukadnak@gmail.com>
2024-01-11 15:27:43 +0900

    Fix body background style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
-      <body className={inter.className}>
+      <body className={`background on-background-text ${inter.className}`}>
         <Providers>{children}</Providers>
       </body>
     </html>
