63819eb3105aa45d640ab8b1bc7b9d3ae8568950
MikuroXina <ryosukadnak@gmail.com>
2024-01-21 01:28:46 +0900

    Use AttendStatus

--- src/organisms/subject-attend.tsx
+++ src/organisms/subject-attend.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { useEffect, useState } from 'react';
-import AttendanceStatus from '../molecules/attendance-status';
+import { useEffect, useState } from "react";
 import { API_ROOT } from "@/queries/config";
 import { useSearchParams } from "next/navigation";
-import styles from './subject-attend.module.css';;
+import styles from "./subject-attend.module.css";
+import AttendStatus from "./attend-status";
 
 type AttendancesSum = {
   onTime: number;
@@ -13,38 +13,46 @@ type AttendancesSum = {
 };
 
 const SubjectAttend = () => {
-    const queryParams = useSearchParams();
-    const subjectId = queryParams.get('subject_id');
-    const [attendance, setAttendance] = useState<AttendancesSum | null>(null);
-  
-    useEffect(() => {
-      const fetchSubjectData = async () => {
-        try {
-          const url = `${API_ROOT}/subjects/${subjectId}/all_attendances`;
-          const response = await fetch(url);
-          if (!response.ok) {
-              return;
-          }
+  const queryParams = useSearchParams();
+  const subjectId = queryParams.get("subject_id");
+  const [attendance, setAttendance] = useState<AttendancesSum | null>(null);
 
-          const data = (await response.json()) as AttendancesSum;
-          setAttendance(data);
-        } catch (error) {
-          console.error('科目データの取得に失敗しました:', error);
+  useEffect(() => {
+    const fetchSubjectData = async () => {
+      try {
+        const url = `${API_ROOT}/subjects/${subjectId}/all_attendances`;
+        const response = await fetch(url);
+        if (!response.ok) {
+          return;
         }
-      };
-  
-      fetchSubjectData();
-    }, [subjectId]);
-  
-    if (!attendance) {
-      return <div className={`surface-variant-text body-medium ${styles.message}`}>科目データを読み込んでいます...</div>;
-    }
-  
+
+        const data = (await response.json()) as AttendancesSum;
+        setAttendance(data);
+      } catch (error) {
+        console.error("科目データの取得に失敗しました:", error);
+      }
+    };
+
+    fetchSubjectData();
+  }, [subjectId]);
+
+  if (!attendance) {
     return (
-      <div>
-        <AttendanceStatus onTime={attendance.onTime} late={attendance.late} miss={attendance.miss} />
+      <div className={`surface-variant-text body-medium ${styles.message}`}>
+        科目データを読み込んでいます...
       </div>
     );
-  };
-  
-  export default SubjectAttend;
+  }
+
+  return (
+    <div className={styles.container}>
+      <AttendStatus
+        attendanceCount={attendance.onTime}
+        tardinessCount={attendance.late}
+        absenceCount={attendance.miss}
+      />
+    </div>
+  );
+};
+
+export default SubjectAttend;
