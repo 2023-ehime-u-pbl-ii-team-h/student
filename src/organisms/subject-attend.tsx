a5798f1221621de2797d0cba5e27127f83417394
i116iked <138637500+i116iked@users.noreply.github.com>
2024-01-20 23:42:56 +0900

    Update src/organisms/subject-attend.tsx
    Co-authored-by: Mikuroさいな <ryosukadnak@gmail.com>

--- src/organisms/subject-attend.tsx
+++ src/organisms/subject-attend.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import AttendanceStatus from '../molecules/attendance-status';
 import { API_ROOT } from "@/queries/config";
 
