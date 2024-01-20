d66f821b7168fbc04f2a0d4bec525b3ee636836a
i116iked <138637500+i116iked@users.noreply.github.com>
2024-01-20 18:10:40 +0900

    Update src/molecules/attendance-status.tsx
    Co-authored-by: Mikuroさいな <ryosukadnak@gmail.com>

--- src/molecules/attendance-status.tsx
+++ src/molecules/attendance-status.tsx
@@ -9,7 +9,7 @@ export type AttendanceStatusProps = {
 
 const AttendanceStatus = ({ onTime, late, miss }: AttendanceStatusProps) => {
   return (
-    <div lassName={styles.attendanceStatus}>
+    <div className={styles.attendanceStatus}>
       <div className={styles.statusItem}>出席: {onTime} 回</div>
       <div className={styles.statusItem}>遅刻: {late} 回</div>
       <div className={styles.statusItem}>欠席: {miss} 回</div>
