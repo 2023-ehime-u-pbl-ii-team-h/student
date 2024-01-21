d1ab784453e3e3b7fbaa2b9d5ccb1ed279ab16e8
MikuroXina <ryosukadnak@gmail.com>
2024-01-22 08:42:18 +0900

    Add registerSubject

---
+++ src/commands/register-subject.ts
@@ -0,0 +1,40 @@
+import { API_ROOT } from "@/queries/config";
+
+export async function registerSubject(subjectId: string): Promise<void> {
+  const registrationRes = await fetch(
+    `${API_ROOT}/me/registrations/${subjectId}`,
+    {
+      method: "POST",
+    },
+  );
+  if (!registrationRes.ok) {
+    throw new Error(
+      `registration of subject (${subjectId}) failed: ${await registrationRes.text()}`,
+    );
+  }
+  const subjectRes = await fetch(`${API_ROOT}/subjects/${subjectId}`);
+  const { boards } = (await subjectRes.json()) as {
+    boards: {
+      id: string;
+      startFrom: string;
+      secondsFromStartToBeLate: number;
+      secondsFromBeLateToEnd: number;
+    }[];
+  };
+
+  const now = Math.floor(Date.now() / 1000);
+  const hasActiveBoard = boards.some(
+    ({ startFrom, secondsFromStartToBeLate, secondsFromBeLateToEnd }) => {
+      const startSec = Math.floor(new Date(startFrom).valueOf() / 1000);
+      return (
+        startSec <= now &&
+        now < startSec + secondsFromStartToBeLate + secondsFromBeLateToEnd
+      );
+    },
+  );
+  if (hasActiveBoard) {
+    location.href = "/";
+  } else {
+    location.href = "/add-subject/";
+  }
+}
